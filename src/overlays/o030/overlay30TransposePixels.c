5d9d5e9afd4a46849ef110260437d3afe99832d8
akratch <kratch.adam@gmail.com>
2026-08-25 17:00:57 +0200

    Match overlay30TransposePixels (near-miss)

--- src/overlays/o030/overlay30TransposePixels.c
+++ src/overlays/o030/overlay30TransposePixels.c
@@ -1,59 +1,33 @@
 #include "PR/ultratypes.h"
 
-typedef struct Overlay30Groups {
-    u8 g0;
-    u8 g1;
-    u8 g2;
-    u8 g3;
-    u8 pad[12];
-} Overlay30Groups;
-
-#ifdef NON_MATCHING
+/* PROVENANCE: adapted from Diddy Kong Racing, src/objects.c (decrypt_magic_codes). */
 void overlay30TransposePixels(u8 *data, s32 length) {
-    volatile Overlay30Groups groups;
-    u8 temp;
-    register s32 count;
-    register u8 *cursor;
-    register s32 i;
+    s32 i;
+    s32 j;
+    u8 *cursor = data;
+    u8 temp[4];
 
-    cursor = data;
-    i = 0;
-    count = length >> 2;
-    if (count > 0) {
-        do {
-            groups.g0 = ((s32)(cursor[2] & 0xC0) >> 4) |
-                        ((s32)(cursor[1] & 0xC0) >> 2) |
-                        (cursor[0] & 0xC0) |
-                        ((s32)(cursor[3] & 0xC0) >> 6);
-            groups.g1 = ((cursor[0] & 0x30) * 4) |
-                        (cursor[1] & 0x30) |
-                        ((s32)(cursor[3] & 0x30) >> 4) |
-                        ((s32)(cursor[2] & 0x30) >> 2);
-            groups.g2 = ((s32)(cursor[3] & 0x0C) >> 2) |
-                        ((cursor[0] & 0x0C) * 0x10) |
-                        ((cursor[1] & 0x0C) * 4) |
-                        (cursor[2] & 0x0C);
-            groups.g3 = (cursor[3] & 3) | (cursor[0] << 6) |
-                        ((cursor[1] & 3) * 0x10) |
-                        ((cursor[2] & 3) * 4);
+    for (i = 0; i < (length >> 2); i++) {
+        temp[0] = (cursor[0] & 0xC0) |
+                  ((cursor[1] & 0xC0) >> 2) |
+                  ((cursor[2] & 0xC0) >> 4) |
+                  ((cursor[3] & 0xC0) >> 6);
+        temp[1] = ((cursor[0] & 0x30) << 2) |
+                  (cursor[1] & 0x30) |
+                  ((cursor[2] & 0x30) >> 2) |
+                  ((cursor[3] & 0x30) >> 4);
+        temp[2] = ((cursor[0] & 0x0C) << 4) |
+                  ((cursor[1] & 0x0C) << 2) |
+                  (cursor[2] & 0x0C) |
+                  ((cursor[3] & 0x0C) >> 2);
+        temp[3] = ((cursor[0] & 3) << 6) |
+                  ((cursor[1] & 3) << 4) |
+                  ((cursor[2] & 3) << 2) |
+                  (cursor[3] & 3);
 
-            temp = groups.g0;
-            cursor[0] = ((temp & 0x55) * 2) |
-                        ((s32)(temp & 0xAA) >> 1);
-            i++;
-            cursor += 4;
-            temp = groups.g1;
-            cursor[-3] = ((temp & 0x55) * 2) |
-                         ((s32)(temp & 0xAA) >> 1);
-            temp = groups.g2;
-            cursor[-2] = ((temp & 0x55) * 2) |
-                         ((s32)(temp & 0xAA) >> 1);
-            temp = groups.g3;
-            cursor[-1] = ((temp & 0x55) * 2) |
-                         ((s32)(temp & 0xAA) >> 1);
-        } while (i != count);
+        for (j = 0; j < 4; j++) {
+            *cursor++ = ((temp[j] & 0xAA) >> 1) |
+                        ((temp[j] & 0x55) << 1);
+        }
     }
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/overlays/o030/overlay30TransposePixels/func_overlay_030_F00002B4_187F1AC.s")
-#endif
