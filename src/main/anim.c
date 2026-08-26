68bc02bdf8fa3e65f29c7481e66a0764105320f7
akratch <kratch.adam@gmail.com>
2026-08-26 12:19:53 +0200

    Salvage uncommitted work (crash 2026-08-26)

--- src/main/anim.c
+++ src/main/anim.c
@@ -555,7 +555,7 @@ void func_80050AD4(u8 pathIndex) {
 void func_80050BF4(void) {
     s32 emptyIndex;
     s32 offset;
-    u8 *cursor;
+    AnimCameraSource **camera;
 
     D_800D6B04 = piRomLoad(0x3D);
     D_800D6B00 = func_8002B280(0x400, 0x81);
@@ -671,7 +671,7 @@ void func_80050DF0(s32 levelId) {
 #ifdef NON_MATCHING
 void func_80050E9C(void) {
     s32 emptyIndex;
-    AnimCameraSource **camera;
+    u8 *cursor;
     AnimScrollReset *scroll;
     AnimLockonReset *lockon;
     AnimLightReset *light;
